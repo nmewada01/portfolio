784eb51eeb2c46e7a2d34dd7ad8c67f49aa88a1f
Naresh Rajput <nareshmewada014@gmail.com>
2022-09-17 18:54:15 +0530

    set the responsiveness

--- src/components/introduction/Intro.jsx
+++ src/components/introduction/Intro.jsx
@@ -1,29 +1,38 @@
 import "./intro.css";
 import profile from "../../img/myProfile.png";
 import { BsFillPlayCircleFill, BsGithub, BsLinkedin } from "react-icons/bs";
-import { Box, Flex, Heading, Image, Text, useMediaQuery } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  Image,
+  Text,
+  useMediaQuery,
+} from "@chakra-ui/react";
 
 const Intro = () => {
-  const [isLargerThan] = useMediaQuery("(min-width: 769px)");
+  const [isLargerThan] = useMediaQuery("(min-width: 430px)");
 
   return (
     <Box className="i" id="intro" lineHeight={"1rem"}>
       <Box className="i-left">
         <Box className="i-left-wrapper">
-          <Text className="i-intro">Hello, My name is</Text>
-          <Heading className="i-name">
+          <Text className="i-intro" my={'5'}>Hello, My name is</Text>
+          <Heading className={isLargerThan ? "i-name" : "typeWritertheme"}>
             Naresh <span style={{ color: "#59b256" }}>Rajput</span>
           </Heading>
-          <Box className="i-title">
-            <Box className="i-title-wrapper">
-              <Box className="i-title-item">Full Stack Developer</Box>
-              <Box className="i-title-item">Chakra UI</Box>
-              <Box className="i-title-item">React</Box>
-              <Box className="i-title-item">Javascript</Box>
-              <Box className="i-title-item">Html/CSS</Box>
+          {isLargerThan ? (
+            <Box className="i-title">
+              <Box className="i-title-wrapper">
+                <Box className="i-title-item">Full Stack Developer</Box>
+                <Box className="i-title-item">Chakra UI</Box>
+                <Box className="i-title-item">React</Box>
+                <Box className="i-title-item">Javascript</Box>
+                <Box className="i-title-item">Html/CSS</Box>
+              </Box>
             </Box>
-          </Box>
-          <Box style={{ display: "flex", gap: "15px", marginBottom: "1rem" }}>
+          ) : null}
+          <Box style={{ display: "flex", gap: "15px", marginBottom: "1rem" }} my={'5'}>
             <Box>
               <a href="https://github.com/nmewada01" target="blank">
                 <BsGithub size="2rem" style={{ color: "blue" }} />
@@ -46,6 +55,7 @@ const Intro = () => {
               className="resume_link"
               href={require("../../resume/Naresh's Resume.pdf")}
               download="Naresh's Resume"
+              my={'5'}
             >
               <Flex alignItems={"center"}>
                 Resume
